871b3df2a31bdea56082ddf93ff87229377bb733
Juan Carlos Vargas <musicaoperativo@gmail.com>
2020-02-06 18:16:33 +0000

    test nro 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,18 @@
 * main - Entry point
 * Return: 0 Always (Success)
 */
-
 int main(void)
 {
-int n;
-rand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is positive\n", n);
-return (0);
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	printf("%d is zero\n", n);
+	else if (n < 0)
+	printf("%d is negative\n", n);
+	else
+	printf("%d is positive\n", n);
+	return (0);
 }
